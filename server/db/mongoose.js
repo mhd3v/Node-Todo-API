8c57bb4abce015ac5acffefd87df80696056d918
mhd3v <mamir3012@gmail.com>
2018-07-23 23:52:24 +0500

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //tell mongoose to use default JS promises
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'); 
+mongoose.connect(process.env.MONGODB_URI); 
                                                                                 
 //we setup a MONGODB_URI with heroku config:set MONGODB_URI=yourUrlHere
 //got URL from mLab. Format -> mongodb://<dbuser>:<dbpassword>@ds141221.mlab.com:41221/todo-api
